e8859d524774813b260731a829ce4a6f8a4f9ebd
masp1234 <mart2c04@stud.kea.dk>
2024-03-22 15:42:31 +0100

    setup-test-database.sh now works regardless of the folder you are currently in

--- scripts/setup-test-database.sh
+++ scripts/setup-test-database.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 container_name=mysql-invadmin
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $script_dir
 
 sudo docker run --name $container_name -e MYSQL_ROOT_PASSWORD=123123 -p 3306:3306 -d mysql
 echo "Starting $container_name..."
 sleep 20
 echo "Container started. Creating database..."
-sudo docker exec -i $container_name mysql -uroot -p123123 < ./db.sql
+sudo docker exec -i $container_name mysql -uroot -p123123 < "$script_dir/db.sql"
 echo "Done!"
