b465874746aeca0dfa5b281c8d93a289e2c4e5ab
masp1234 <mart2c04@stud.kea.dk>
2024-03-26 14:22:47 +0100

    test

--- messages/product.js
+++ messages/product.js
@@ -1,6 +1,9 @@
 import connect from './connection.js'
 
 async function publishProductEvent(product, productStatus) {
+    if (!product || !productStatus) {
+        throw new Error('Invalid parameters: need to have both product and productStatus arguments')
+    }
         const { channel, exchange} = await connect();
         const message = {
             status: productStatus,
